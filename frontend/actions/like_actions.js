8929d40bf037887a0aaa7d934374d17eb59b0a94
App Academy Student <student@appacademy.io>
2020-03-12 09:16:38 -0700

    Added like actions

---
+++ frontend/actions/like_actions.js
@@ -0,0 +1,26 @@
+import * as LikeAPIUtil from;
+
+export const CREATE_LIKE = 'CREATE_LIKE';
+export const REMOVE_LIKE= 'REMOVE_LIKE';
+
+
+const createSingleLike = like => ({
+  type: CREATE_LIKE,
+  like
+});
+
+const removeLike = likeId => ({
+  type: REMOVE_LIKE,
+  likeId
+});
+
+
+export const createLike= like => dispatch => (
+  LikeApiUtil.createLike(like)
+    .then(like => dispatch(createSingleLike(like)))
+);
+
+export const deleteLike = likeId => dispatch => (
+  LikeApiUtil.deleteLike(likeId)
+    .then(() => dispatch(removeLike(likeId)))
+);

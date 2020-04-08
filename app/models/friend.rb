823580b47360e16af70e3d20d6eb0dbac51a0b2d
irynamcb <iryna.mcbride@gmail.com>
2020-04-07 18:47:46 -0700

    Add validations to friend_request model

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -7,17 +7,4 @@ primary_key: :id,
 foreign_key: :user_id,
 class_name: :User
 
-
-# def not_self
-#   errors.add(:friend, "can't be equal to user") if user == friend
-# end
-
-# def not_friends
-#   errors.add(:friend, 'is already added') if user.friends.include?(friend)
-# end
-
-# def not_pending
-#   errors.add(:friend, 'already requested friendship') if friend.pending_friends.include?(user)
-# end
-
 end
--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -1,6 +1,7 @@
 class FriendRequest < ApplicationRecord
 
 validates :user_id, :friend_id, presence: true
+validate :not_self, :not_friends
 
 belongs_to :user,
 primary_key: :id,
@@ -12,11 +13,20 @@ primary_key: :id,
 foreign_key: :friend_id,
 class_name: :User
 
+def not_self
 
-private
+ if current_user.id == friend_id
+    errors[:friend_id] << "Cant add yourself to friends"
+ end
 
-# def not_self
-#   errors.add(:friend, "can't be equal to user") if user == friend
-# end
+end
+
+def not_friends
+
+  if  Friend.find_by(user_id: current_user.id, friend_id: friend_id)
+
+    errors[:friend_id] << "This friendship already exists"
+  end
+end
 
 end

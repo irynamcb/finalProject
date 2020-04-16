da996ab036a2cda9ee7655d9f90e0a31e09b869d
irynamcb <iryna.mcbride@gmail.com>
2020-04-15 21:34:39 -0700

    Add search controller

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -1,4 +1,17 @@
 class Api::SearchController < ApplicationController
 
+
+    def search
+
+        # @users = User.where("first_name LIKE ? ", "%#{params[:search_key]}%")
+        @users = User.where("first_name LIKE ? ", "Br%")
+         if @users
+            render :search
+        else 
+            flash.now[:errors] = ['Cant find anything']
+            render json: ['Cant find anything'], status: :not_found
+        end
+
+    end
     
 end
